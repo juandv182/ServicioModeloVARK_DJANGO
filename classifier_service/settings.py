c20f5a5e742dd9b955be1ac17fa39776ab857b0e
juandv182 <a20170089@pucp.edu.pe>
2024-06-19 20:23:01 -0500

    ready for render

--- classifier_service/settings.py
+++ classifier_service/settings.py
@@ -83,23 +83,6 @@ WSGI_APPLICATION = 'classifier_service.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'sqlite3': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     },
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'aulax',
-#         'USER': 'root',
-#         'PASSWORD': '14652213Jd_',
-#         'HOST': 'localhost',
-#         'PORT': '3306',
-#         'OPTIONS': {
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",  # Ajusta según sea necesario
-#         },
-#     }
-# }
 DATABASES = {
     'sqlite3': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -107,16 +90,33 @@ DATABASES = {
     },
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT', '3306'),
+        'NAME': 'aulax',
+        'USER': 'root',
+        'PASSWORD': '14652213Jd_',
+        'HOST': 'localhost',
+        'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",  # Ajusta según sea necesario
         },
     }
 }
+# DATABASES = {
+#     'sqlite3': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     },
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': os.environ.get('DB_NAME'),
+#         'USER': os.environ.get('DB_USER'),
+#         'PASSWORD': os.environ.get('DB_PASSWORD'),
+#         'HOST': os.environ.get('DB_HOST'),
+#         'PORT': os.environ.get('DB_PORT', '3306'),
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",  # Ajusta según sea necesario
+#         },
+#     }
+# }
 
 
 # Password validation
