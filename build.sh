82b246d71bde330483fd1680a2cc7e6fc5451a71
juandv182 <a20170089@pucp.edu.pe>
2024-06-19 19:29:42 -0500

    ready for ending

--- build.sh
+++ build.sh
@@ -8,5 +8,12 @@ pip install -r requirements.txt
 # Convert static asset files
 python manage.py collectstatic --no-input
 
+python manage.py makemigrations --empty auth
+python manage.py makemigrations --empty admin
+python manage.py makemigrations --empty contenttypes
+python manage.py makemigrations --empty sessions
+python manage.py makemigrations --empty messages
+python manage.py makemigrations --empty staticfiles
+python manage.py makemigrations staticfiles
 # Apply any outstanding database migrations
-python manage.py migrate
+python manage.py migrate classifier
